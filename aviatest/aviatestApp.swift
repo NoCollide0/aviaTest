0844fc9fc22379787ee5f74ea8552ed6bfbf1ba5
2132112313 <2131>
2024-05-29 18:44:42 +0300

    Initial Commit

---
+++ aviatest/aviatestApp.swift
@@ -0,0 +1,17 @@
+//
+//  aviatestApp.swift
+//  aviatest
+//
+//  Created by Федор Шашков on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct aviatestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
